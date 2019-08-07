7a9503922aac5cfdbcbcd0cac55e2524a8e0c618
rabpeter <de.nimitz@yandex.com>
2019-08-07 18:31:05 -0500

    demo version

---
+++ README.md
@@ -0,0 +1,15 @@
+# Clarifai Face DEMO App
+
+## This project finds faces in an image and overlays an emoji (or text) of your choice.
+
+## Get the project set up
+
+1. Go to the Clarifai website, sign up for an account and create a new application.
+2. Copy your Client ID and copy it into the cId variable in secret.js
+3. Copy your Client Secret and copy it into the cSec variable in secret.js
+4. Run ```npm install```
+5. For development run ```npm start```
+6. To create production build run ```npm build``` and got for ```dist``` folder. Open ```index.html``` in your favourite browser
+
+
+*Note: Do not share your Client ID or Secret. The file secret.js is already included in the .gitignore file, meaning it will not be committed to your git repo.*
---
+++ src/index.js
@@ -0,0 +1,144 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Clarifai from 'clarifai'
+import { API_KEY } from '../secret'
+import './style.css'
+
+const app = new Clarifai.App({
+  apiKey: API_KEY
+})
+
+// Overlaid on faces
+const overlay = 'F'
+
+class Home extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      imgInfo: {
+        b64: '',
+        clarifaiFaces: [],
+        realFaces: []
+      }
+    }
+    this.canvasRef = React.createRef(null)
+  }
+
+  // Draw a HTML Canvas and make it the the same size as the image
+  drawBoxes = () => {
+    console.log('Start draw boxes on canvas')
+    const { imgInfo } = this.state
+    const newImgInfo = imgInfo
+    const image = new Image();
+    const ctx = this.canvasRef.getContext('2d')
+    image.onload = () => {
+      ctx.drawImage(image, 0, 0, imgInfo.width, imgInfo.height)
+      // const ctx = canvas.getContext('2d')
+      ctx.textBaseline = 'top'
+
+      // For each bounding box we calculate the 'real' coordinates (x, y, width, height) of the faces and push the overlay emoji on top of it
+      for (let i = 0; i < newImgInfo.clarifaiFaces.length; i++) {
+        const box = {
+          x: newImgInfo.clarifaiFaces[i].left_col * newImgInfo.width,
+          y: newImgInfo.clarifaiFaces[i].top_row * newImgInfo.height,
+          w: (newImgInfo.clarifaiFaces[i].right_col * newImgInfo.width) - (newImgInfo.clarifaiFaces[i].left_col * newImgInfo.width),
+          h: (newImgInfo.clarifaiFaces[i].bottom_row * newImgInfo.height) - (newImgInfo.clarifaiFaces[i].top_row * newImgInfo.height)
+        }
+        newImgInfo.realFaces.push(box)
+        ctx.beginPath()
+        ctx.rect(box.x, box.y, box.w, box.h)
+        ctx.lineWidth = 4
+        ctx.strokeStyle = 'blue'
+        ctx.stroke()
+
+        this.setState({
+          imgInfo: newImgInfo
+        })
+        // ctx.font = (box.w * 1.4) + 'px monospace'
+        // ctx.font = '3px monospace'
+        // ctx.fillText(overlay, box.x - (box.w / 5), box.y - (box.h / 4))
+      }
+    }
+    image.src = imgInfo.b64
+  }
+
+  // Face detection alpha model and pushes the bounding boxes to imgInfo.clarifaiFaces
+  faceDetection = (b64Img) => {
+    console.log('Start face detection')
+    const { imgInfo } = this.state
+    const newImgInfo = imgInfo
+    app.models.predict('a403429f2ddf4b49b307e318f00e528b', {
+      base64: b64Img
+    }).then(
+      (res) => {
+        const data = res.outputs[0].data.regions
+        if (data !== null) {
+          for (let i = 0; i < data.length; i++) {
+            newImgInfo.clarifaiFaces.push(data[i].region_info.bounding_box)
+          }
+        }
+        this.setState({
+          imgInfo: newImgInfo
+        }, () => this.drawBoxes())
+      },
+      (err) => {
+        console.log(err)
+      }
+    )
+  }
+
+  // When there's an image in the input, grab the file and do the following:
+  // 1. Convert image to Base-64 and store in imgInfo.b64
+  // 2. Populate the image tag with the Base-64 string
+  // 3. Create a copy of the Base-64 string which can be sent to Clarifai in imgInfo.b64Clarifai
+  imageUpload = (e) => {
+    console.log('Start uploading image')
+    const { imgInfo } = this.state
+    const newImgInfo = imgInfo
+    const files = Array.from(e.target.files)
+    if (files[0]) {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        newImgInfo.b64 = e.target.result
+        newImgInfo.b64Clarifai = newImgInfo.b64.replace(/^data:image\/(.*);base64,/, '')
+        this.setState({
+          imgInfo: newImgInfo
+        }, () => this.faceDetection(newImgInfo.b64Clarifai))
+      }
+      reader.readAsDataURL(files[0])
+    }
+  }
+
+  onLoadImg = ({ target:img }) => {
+    const { imgInfo } = this.state
+    const newImgInfo = imgInfo
+    newImgInfo.width = img.offsetWidth
+    newImgInfo.height = img.offsetHeight
+
+    this.setState({
+      imgInfo: newImgInfo
+    })
+  }
+
+  render () {
+    const { imgInfo } = this.state
+    console.log('imgInfo: ', imgInfo)
+
+    return (<div className='home'>
+      <div className='input-group'>
+        <label htmlFor='image'>Choose an image</label>
+        <input type='file' id='image' onChange={this.imageUpload} />
+      </div>
+      <div className='face-results'>
+        <div className='col'>
+          <img src={imgInfo.b64} onLoad={this.onLoadImg} />
+        </div>
+        <div className='col'>
+          <canvas id='canvas' ref={canvasRef => this.canvasRef = canvasRef} width={imgInfo.width} height={imgInfo.height} />
+        </div>
+      </div>
+    </div>)
+  }
+}
+
+ReactDOM.render(<Home />, document.querySelector('#root'))
